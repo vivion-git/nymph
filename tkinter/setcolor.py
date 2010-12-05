d88bfd3ac2315b4b461c5daf5858a144fc5fc5d6
Vivion <vivion@gmail.com>
2010-12-05 23:44:59 +0800

    explain the result of the  setcolor

--- tkinter/setcolor.py
+++ tkinter/setcolor.py
@@ -10,3 +10,6 @@ push=Button(root,text='set Background Color',command=setBgColor)
 push.config(height=3,font=('times',20,'bold'))
 push.pack(expand=YES,fill=BOTH)
 root.mainloop()
+
+note:ther script is run ,when you choose the color in the askcolor dialog,the
+background of the Button will be changed.
